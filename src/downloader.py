33553086c4ba3227e3753b202c1127cb7ece8e56
Brian Filarsky <bfilarsky@gmail.com>
2021-11-22 18:40:00 -0800

    flake8

--- src/downloader.py
+++ src/downloader.py
@@ -4,8 +4,9 @@ import os
 
 BASE_ENDPOINT = 'http://api.511.org/transit/'
 
+
 def download_data(api, **kwargs):
     load_dotenv()
-    api_key=os.environ.get("api_key")
+    api_key = os.environ.get("api_key")
     endpoint = BASE_ENDPOINT + api + '?api_key=' + api_key
     return requests.get(endpoint)
