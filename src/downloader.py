9596317965d045023f3bf78897bd00f3ce81214f
Brian Filarsky <bfilarsky@gmail.com>
2021-11-23 17:06:56 -0800

    Remove CI dependence on real API key, split download function

--- src/downloader.py
+++ src/downloader.py
@@ -2,11 +2,31 @@ import requests
 from dotenv import load_dotenv
 import os
 
-BASE_ENDPOINT = 'http://api.511.org/transit/'
+__BASE_ENDPOINT = 'http://api.511.org/transit/'
 
 
-def download_data(api, **kwargs):
+def createEndpoint(api, api_key, **kwargs):
+    endpoint = __BASE_ENDPOINT + api + '?api_key=' + api_key
+    for key, value in kwargs.items():
+        endpoint = endpoint + '&' + key + '=' + value
+    return endpoint
+
+
+def downloadData(endpoint):
+    data = requests.get(endpoint)
+    if (data.status_code == 200):
+        return data.text
+    else:
+        return None
+
+
+def getApiKey():
     load_dotenv()
-    api_key = os.environ.get("api_key")
-    endpoint = BASE_ENDPOINT + api + '?api_key=' + api_key
-    return requests.get(endpoint)
+    return os.environ.get("api_key")
+
+
+def downloadJSON(api, **kwargs):
+    kwargs['api_key'] = getApiKey()
+    kwargs['format'] = 'json'
+    endpoint = createEndpoint(api, **kwargs)
+    return downloadData(endpoint)
--- src/test_downloader.py
+++ src/test_downloader.py
@@ -2,6 +2,48 @@
 import downloader
 
 
-def test_downloader():
-    data = downloader.download_data("operators")
-    assert data.status_code == 200
+def testCreateEndpointNoArgs():
+    test_api = 'test_api'
+    test_api_key = '123456789'
+    endpoint = downloader.createEndpoint(test_api, test_api_key)
+    assert endpoint == 'http://api.511.org/transit/test_api?api_key=123456789'
+
+
+def testCreateEndpointSingleArg():
+    test_api = 'test_api'
+    test_api_key = '123456789'
+    endpoint = downloader.createEndpoint(test_api, test_api_key, test_key='arg')
+    assert endpoint == 'http://api.511.org/transit/test_api?api_key=123456789&test_key=arg'
+
+
+def testCreateEndpointTwoArgs():
+    test_api = 'test_api'
+    test_api_key = '123456789'
+    endpoint = downloader.createEndpoint(test_api, test_api_key, test_key='arg', test_key2='arg2')
+    assert endpoint == 'http://api.511.org/transit/test_api?api_key=123456789&test_key=arg&test_key2=arg2' \
+        or endpoint == 'http://api.511.org/transit/test_api?api_key=123456789&test_key2=arg2&test_key=arg'
+
+
+def testdownloadDataNominal(requests_mock):
+    mock_endpoint = 'http://test_endpoint/test_api'
+    requests_mock.get(mock_endpoint, json={'test_key': 'test_value'})
+    data = downloader.downloadData(mock_endpoint)
+    assert data == '{"test_key": "test_value"}'
+
+
+def testdownloadDataError(requests_mock):
+    mock_endpoint = 'http://test_endpoint/test_api'
+    requests_mock.get(mock_endpoint, status_code=400)
+    data = downloader.downloadData(mock_endpoint)
+    assert data is None
+
+
+def testDownloader(requests_mock):
+    endpoint = downloader.createEndpoint('test_api', api_key=downloader.getApiKey())
+    requests_mock.get(endpoint, json={'test_key': 'test_value'})
+    data = downloader.downloadJSON('test_api')
+    assert data == '{"test_key": "test_value"}'
+
+
+if __name__ == "__main__":
+    print(downloader.downloadJSON("tripupdates", agency='SF').text)
