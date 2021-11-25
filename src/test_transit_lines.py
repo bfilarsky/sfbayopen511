e2835e73beab1306ed854f62480a296115eefae8
Brian Filarsky <9205921+bfilarsky@users.noreply.github.com>
2021-11-25 12:22:32 -0800

    Add transit lines (#9)

---
+++ src/test_transit_lines.py
@@ -0,0 +1,29 @@
+from transit_lines import TransitLines
+
+import json
+from unittest.mock import patch
+import pytest
+
+
+@patch('transit_lines.download')
+def test_parse_server_error(mock_download):
+    mock_download.return_value = None
+    with pytest.raises(ConnectionError):
+        TransitLines('123', 'SF')
+
+
+@patch('transit_lines.download')
+def test_parse_nominal(mock_download):
+    with open('test_data/lines_nominal.json') as file:
+        mock_download.return_value = json.load(file)
+    transit_lines = TransitLines('123', 'SF')
+    assert len(transit_lines.lines) == 5
+    for idx, operator in enumerate(transit_lines.lines):
+        assert operator.name == "Line " + str(idx)
+        assert operator.id == str(idx)
+        if idx % 2 == 1:
+            assert operator.monitored is True
+            assert operator.transport_mode == "metro"
+        else:
+            assert operator.monitored is False
+            assert operator.transport_mode == "bus"
