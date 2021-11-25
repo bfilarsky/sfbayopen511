cb81cec28fecc21fc7ca953755abd0ec13004c4c
Brian Filarsky <9205921+bfilarsky@users.noreply.github.com>
2021-11-25 11:51:55 -0800

    Add integration tests (#8)

---
+++ src/test_transit_operators_schema.py
@@ -0,0 +1,24 @@
+from jsonschema import validate
+import json
+from dotenv import load_dotenv
+import os
+
+from downloader import download
+
+
+def test_nominal_schema():
+    with open('test_data/operators_schema.json') as schema, open('test_data/operators_nominal.json') as nominal:
+        validate(instance=json.load(nominal), schema=json.load(schema))
+
+
+def test_fixed_schema():
+    with open('test_data/operators_schema.json') as schema, open('test_data/operators_fixed_monitored.json') as nominal:
+        validate(instance=json.load(nominal), schema=json.load(schema))
+
+
+def test_live_schema():
+    load_dotenv()
+    api_key = os.environ.get("api_key")
+    transit_operators_live = download('operators', api_key)
+    with open('test_data/operators_schema.json') as schema:
+        validate(instance=transit_operators_live, schema=json.load(schema))
